025a1422a6e0c934fa9f5ba87da5480d88984b65
KazitoAz <101050064@student.swin.edu.au>
2018-09-21 20:42:25 +1000

    Fix follow design
    Improve the interface

--- show_inventory.php
+++ show_inventory.php
@@ -1,55 +1,91 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
- <meta charset="utf-8" />
- <meta name="description" content="Display Inventory details" />
- <meta name="keywords" content="display" />
- <title>Show Inventory</title>
+<title>Show Inventory</title>
+
+<meta charset="utf-8" />
+<meta name="description" content="show inventory page"  />
+<meta name="keywords" content="PHP" />
+
+
 </head>
 <body>
-<h1>Inventory</h1>
-<?php
+	<?php
+    include("hearder.inc");
+	include("nav.inc");
+	?>
+	
+	<form method="post" action="show_inventory.php">
+	<fieldset>
+	<legend>Search Inventory by product name</legend>
+		<p class="row">	<label for="product_name">Product Name: </label>
+			<input type="text" name="product_name" id="product_name" /></p>
+	
+		<p>	<input type="submit" value="Search Inventory" /></p>
+	
+	</fieldset>
+	<?php
     require_once ("settings.php");
 	
 	$conn = @mysqli_connect($host,
-	  $user, 
-	  $pwd, 
-	  $sql_db
+	    $user,
+		$pwd,
+		$sql_db
 	);
 	
-	if (!$conn) {
-		echo "<p>Database connection failure</p>";
-	} else {
-		$sql_table="Inventory";
-		$query = "select product_name, quantity, supplier, contact_number, address FROM Inventory ORDER BY product_name";
-		$result = mysqli_query($conn, $query);
-		
-		if(!$result){
-			echo "<p>Something is wrong with ", $query, "</p>";
+	$name = trim($_POST["product_name"]);
+	$quantity = trim($_POST["quantity"]);
+	$price = trim($_POST["price"]);
+	$date = trim($_POST["date"]);
+	
+	$query = "insert into $sql_table (name, quantity, price, date) values ('$name', '$quantity', '$price', '$date')";
+	
+	    $result = mysqli_query($conn, $query);
+		if(!$result) {
+			echo "<p class=\"wrong\">Something is wrong with",   $query,"</p>";
 		} else {
-			echo "<table border=\"1\">\n";
-			echo "<tr>\n "
-			    ."<th scope=\"col\">Product Name</th>\n"
-				."<th scope=\"col\">Quantity</th>\n"
-				."<th scope=\"col\">Supplier</th>\n"
-				."<th scope=\"col\">Contact Number</th>\n"
-				."<th scope=\"col\">Address</th>\n"
-				."</tr>\n";
-				
-			while ($row = mysqli_fetch_assoc($result)){
-				echo "<tr>\n";
-				echo "<td>",$row["product_name"],"</td>\n";
-				echo "<td>",$row["quantity"],"</td>\n";
-				echo "<td>",$row["supplier"],"</td>\n";
-				echo "<td>",$row["contact_number"],"</td>\n";
-				echo "<td>",$row["address"],"</td>\n";
-				echo "</tr>\n";
-			}
-		echo "</table>\n";
-		mysqli_free_result($result);
+			echo "<p class=\"ok\">Successfully for Inventory</p>";
 		}
+		
 		mysqli_close($conn);
-	}
-?>
+	?>
+	</form>
+
+  <table>
+  <thead>
+   <tr>
+    <th>Product Name</th>
+	<th>Quantity</th>
+	<th>Price</th>
+	<th>Date</th>
+   </tr>
+  </thead>
+  <tbody>
+   <tr>
+    <th> </th>
+    <td> </td>
+	<td> </td>
+	<td> </td>
+	<td> </td>
+	<td> </td>
+   </tr>
+   <tr>
+    <th> </th>
+    <td> </td>
+	<td> </td>
+	<td> </td>
+	<td> </td>
+	<td> </td>
+   </tr>
+  </tbody>
+  </table>
+
+
+  <fieldset>
+  	<legend>Aleart Listing</legend>
+  	
+  </fieldset>
+
+
 </body>
 </html>

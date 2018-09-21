493d3acc8a83a4e2eddace14fe37be993287307d
KazitoAz <101050064@student.swin.edu.au>
2018-09-21 21:39:34 +1000

    Improve

--- view_sales.php
+++ view_sales.php
@@ -1,42 +1,86 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<title>Show Weekly Sales Report</title>
-
-<meta charset="utf-8" />
-<meta name="description" content="show weekly sales record html page"  />
-<meta name="keywords" content="connect the database and html page" />
-<link href="style/style.css" rel="stylesheet"/>
+ <meta charset="utf-8" />
+ <meta name="description" content="Display Weekly sales record" />
+ <meta name="keywords" content="display" />
+ <title>Display Weekly Sales Record</title>
+ <link href="style/style.css" rel="stylesheet"/>
   <script src="scripts/showSales.js"></script>
-
-
 </head>
 <body>
-<?php include_once "header.inc"; ?>
-	<h1>Show Weekly Sales Report</h1>
-		<?php
+<?php
+    include("hearder.inc");
+	include("nav.inc");
+	?>
 	
-	include_once "nav.inc"
-		?>
-	<form method="post" action="show_weekly_salesreport.php">
+	<form method="post" action="show_weekly_sales_record.php">
 	<fieldset>
-	<legend>Select week for showing</legend>
-		<p class="row">	<label for="week">number of week: </label>
+	<legend>Search the week for showing</legend>
+		<p class="row">	<label for="week">The number of week:</label>
 			<input type="text" name="week" id="week" /></p>
-		
-		<p>	<input type="submit" value="Confirm week" /></p>
 	
-	<h2>Or you can choose date</h2>
-	
-	<legend>Select week for showing</legend>
-		<p class="row">	<label for="begin_date">The begin date: </label>
-			<input type="text" name="begin_date" id="begin_date" /></p>
-		<p class="row">	<label for="end_date">The end date: </label>
-			<input type="text" name="end_date" id="end_date" /></p>
-		
-		<p>	<input type="submit" value="Confirm date" /></p>
+		<p>	<input type="submit" value="Search Sales Record" /></p>
 	
 	</fieldset>
+	<?php
+    include_once("db.php");
+	
+	$name = trim($_POST["product_name"]);
+	$quantity = trim($_POST["quantity"]);
+	$supplier = trim($_POST["supplier"]);
+	$contact = trim($_POST["contact"]);
+	$address = trim($_POST["address"]);
+	
+	$query = "insert into $sql_table (product_name, quantity, supplier, contact, address) values ('$name', '$quantity', '$supplier', '$contact', 'address')";
+	
+	    $result = mysqli_query($conn, $query);
+		if(!$result) {
+			echo "<p class=\"wrong\">Something is wrong with",   $query,"</p>";
+		} else {
+			echo "<p class=\"ok\">Successfully searching weekly sales record</p>";
+		}
+		
+		mysqli_close($conn);
+	?>
 	</form>
+
+  <table>
+  <thead>
+   <tr>
+    <th>Product Name</th>
+	<th>Quantity</th>
+	<th>Supplier</th>
+	<th>Contact</th>
+	<th>Address</th>
+   </tr>
+  </thead>
+  <tbody>
+   <tr>
+    <th> </th>
+    <td> </td>
+	<td> </td>
+	<td> </td>
+	<td> </td>
+	<td> </td>
+	<td> </td>
+   </tr>
+   <tr>
+    <th> </th>
+    <td> </td>
+	<td> </td>
+	<td> </td>
+	<td> </td>
+	<td> </td>
+	<td> </td>
+   </tr>
+  </tbody>
+  </table>
+
+
+
+
+</body>
+</html>
 </body>
 </html>

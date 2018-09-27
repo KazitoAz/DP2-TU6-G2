d1882bf57b1c1d3172dafadd7f103d52f23d8843
Maximus Ooi <maximusooi8298@gmail.com>
2018-09-27 15:15:49 +1000

    Index
    Renamed homepage to index for better compatibility with XAMPP (htdocs)

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  
+  <link href="" rel="stylesheet" />
+  <meta charset="utf-8" />
+  <meta name="description" content="HomePage" />
+  <meta name="keywords" content="HTML5, CSS layout" />
+  <meta name="author" content="XUANZHE FENG"  />
+  <title>Home Page</title>
+</head>
+
+<body>
+ <?php 
+ include_once "header.inc";
+include_once "nav.inc"
+ ?>
+	<article>
+	    <section id="info">
+		    <h2>Basic information</h2>
+		    <p>This application is used to........</p>
+	
+	    </section>
+	
+	    
+	</article>
+	
+	
+</body>
+
+</html>

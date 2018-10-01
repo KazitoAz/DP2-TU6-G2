1d6d6b2c419f501fa1017753f102c5f66b0acfb8
KazitoAz <101050064@student.swin.edu.au>
2018-10-01 23:53:29 +1000

    Predict monthly sales record front end

---
+++ predict_monthly_sales_record_fe.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset='utf-8'/>
+		<meta name='author' content='XUANZHE FENG'/>
+		<link href="style/style.css" rel="stylesheet"/>
+		<script src="scripts/AddInventory.js"></script>
+		<title>Predict Monthly Sales Record</title>
+	</head>
+   <body>
+<?php
+include_once "header.inc";
+include_once "nav.inc";
+?>
+
+		<form action="Predict_monthly_sales_record.php" method="post">
+			<fieldset>
+				<p>
+				<label for='ItemName'>Choose Item that you want</label>
+				<p>
+					<label for='ItemName'>Item</label>
+					<input id='ItemName' name='ItemName' type='text' required='required' pattern="^[a-zA-Z]+$"/>
+				</p>
+				
+				
+				<p>
+				<span class="errortxt" id="statetxt"></span>
+				</p>
+				<p>
+    			<button id="predict" class="btn btn-warning" type="submit" name="predict" value="predict">Predict Monthly Sales Record</button>				
+				<input type="reset" value="Reset"/>
+				</p>
+			</fieldset>
+		</form>
+   </body>
+</html>

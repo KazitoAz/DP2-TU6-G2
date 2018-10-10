71e876a3a27b7f6a613b3d2be4a292a6d50e31d0
KazitoAz <101050064@student.swin.edu.au>
2018-10-10 16:19:34 +1100

    Finish alert inventory page

---
+++ alert.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset='utf-8'/>
+		<meta name='author' content='XUANZHE FENG'/>
+		<link href="style/style.css" rel="stylesheet"/>
+		<script src="scripts/AddInventory.js"></script>
+		<title>Alert inventory</title>
+	</head>
+   <body>
+<?php
+include_once "header.inc";
+include_once "nav.inc";
+
+include_once("db.php"); 
+           $q = $_POST["quantity"];
+
+           $query="Select productname, quantity as "Alert ones" from inventory WHERE quantity < 10 ";
+?>
+
+
+
+   </body>
+</html>
